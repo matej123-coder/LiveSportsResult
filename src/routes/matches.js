4ff2d72ea15a5dcd7c1bcaa81e80caf94bac3919
matej123-coder <mmatej.minoski@gmail.com>
2026-08-10 16:13:23 +0200

    feat: create match

---
+++ src/routes/matches.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { createMatchSchema } from "../validation/matches.js";
+import {matches} from "../db/schema.js"
+import {db} from "../db/db.js"
+import { getMatchStatus } from "../utils/match-status.js";
+export const matchRouter = Router();
+
+matchRouter.get("/",(req,res)=>{
+    res.status(200).json({message: "Matches List"})
+})
+matchRouter.post("/", async (req,res)=>{
+    const parsed = createMatchSchema.safeParse(req.body);
+    if(!parsed.success){
+    return  res.status(400).json({
+            error: "Invalid payload",
+            details: JSON.stringify(parsed.error)
+        })
+    }
+    const { data: {startTime,endTime,homeScore,awayScore}} = parsed;
+    
+    try{
+        const [event] = await db.insert(matches).values({
+            ...parsed.data,
+            startTime: new Date(startTime),
+            endTime: new Date(endTime),
+            homeScore: homeScore ?? 0,
+            awayScore: awayScore ?? 0,
+            status: getMatchStatus(startTime,endTime)
+        }).returning();
+
+        res.status(201).json({
+            data: event,
+
+        })
+    }  
+    catch(e){
+        res.status(500).json({
+            error: "Failed to create match",
+            details: JSON.stringify(e)
+        })
+    }
+})
