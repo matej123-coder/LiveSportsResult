05d0ab00cae5c5fb726e2349fd7a5ea6631babd8
matej123-coder <mmatej.minoski@gmail.com>
2026-08-11 09:51:56 +0200

    hotfix

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -14,7 +14,7 @@ matchRouter.get("/",async (req,res)=>{
     if(!parsed.success){
     return  res.status(400).json({
             error: "Invalid query",
-            details: JSON.stringify(parsed.error)
+            details: parsed.error.issues
         })
     }
     const limit = Math.min(parsed.data.limit ?? 50,MAX_LIMIT)
@@ -34,7 +34,7 @@ matchRouter.post("/", async (req,res)=>{
     if(!parsed.success){
     return  res.status(400).json({
             error: "Invalid payload",
-            details: JSON.stringify(parsed.error)
+            details:  parsed.error.issues
         })
     }
     const { data: {startTime,endTime,homeScore,awayScore}} = parsed;
