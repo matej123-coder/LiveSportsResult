0e0c77e1cd93e0a200b4a580e77f3a4dd9b65605
matej123-coder <mmatej.minoski@gmail.com>
2026-08-17 15:33:35 +0200

    feat: add frontend

--- frontend/LiveScore/src/App.tsx
+++ frontend/LiveScore/src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <section id="center">
+    <div>Hello </div>
+      {/* <section id="center">
+        <p>Hello</p>
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
@@ -114,7 +114,7 @@ function App() {
       </section>
 
       <div className="ticks"></div>
-      <section id="spacer"></section>
+      <section id="spacer"></section> */}
     </>
   )
 }
